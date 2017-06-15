10a190b03ea6a3e82d0e7efa235467360c74ab53
stiekel <histkc@gmail.com>
2017-06-16 07:11:23 +0800

    [front] [add] get camera

--- front/src/components/live/index.vue
+++ front/src/components/live/index.vue
@@ -1,16 +1,38 @@
 <template lang="jade">
   div
     h1 Live
+    video#live(autoplay)
 </template>
 
 <script>
 export default {
   name: 'Live',
+  methods: {
+    success (stream) {
+      let ele = document.getElementById('live')
+      ele.srcObject = stream
+    },
+    failed (err) {
+      console.error('getUserMedia failed:', err.name, err)
+    },
+    load () {
+      window.navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true,
+        screen: false
+      }).then(this.success).catch(this.failed)
+    }
+  },
   mounted () {
-    console.log('mounted')
+    this.load()
   }
 }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
+video
+  width: 720px;
+@media screen and (max-width: 720px)
+  video
+    width: 100%;
 </style>
